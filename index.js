3589aaafe63365d2a4f665c30bbe25cfba9eb5de
Abhijit Radhakrishnan <peringadanabhijit@gmail.com>
2024-09-24 18:19:47 +0530

    1

---
+++ index.js
@@ -0,0 +1,5 @@
+import express from 'express';
+// const express = require('express');      // this statement is similar as the above one
+import bodyParser from 'body-parser';
+
+const app = express();
